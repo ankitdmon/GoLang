c8387dddd38bafafad21433ebdf702f8129a0cca
Ankit-Flutch <93345314+ankitdmon@users.noreply.github.com>
2023-10-02 21:36:14 +0530

    Basic REST API setup.

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func main(){
-	fmt.Println ("Hello world!!");
+func getMessage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Hello, World!!")
+}
+
+func main() {
+	http.HandleFunc("/api", getMessage)
+
+	fmt.Println("Server is running on PORT: 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 }
