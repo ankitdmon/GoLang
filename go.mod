9cd16cffbc28f09729f5aec31966fc01fe4dbb07
Ankit-Flutch <93345314+ankitdmon@users.noreply.github.com>
2023-10-02 22:04:53 +0530

    Added GorillaMUX

---
+++ go.mod
@@ -0,0 +1,5 @@
+module GOLANG
+
+go 1.21.1
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func getMessage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "Hello, World!!")
+	fmt.Fprintln(w, "Hello, World!!")
 }
 
 func main() {
-	http.HandleFunc("/api", getMessage)
+	router := mux.NewRouter()
+
+	// Define the route using the Gorilla Mux router
+	router.HandleFunc("/test", getMessage).Methods("GET")
 
 	fmt.Println("Server is running on PORT: 8080")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
